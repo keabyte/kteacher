de6b9342252f16ceb8b49746a3bfdffeceb3cdfd
Taylar <keast.taylar@gmail.com>
2023-12-21 12:40:31 +1300

    feat(flash cards): flash card loop

--- src/components/flash-card-game/FlashCard.jsx
+++ src/components/flash-card-game/FlashCard.jsx
@@ -1,7 +1,7 @@
 import './FlashCard.css';
 
 const FlashCard = ({character, showRoman}) => {
-  return <div className="cheat-sheet-tile">
+  return <div className="flash-card-tile">
   <div className="text-6xl">{character.hangul}</div>
   {showRoman && <div className="text-xl pt-2 font-bold">{character.roman}</div> }
 </div>;
--- src/components/flash-card-game/FlashCardGame.jsx
+++ src/components/flash-card-game/FlashCardGame.jsx
@@ -3,40 +3,102 @@ import hangul from '../../hangul.json';
 import FlashCard from "./FlashCard";
 import { FlashCardAnswer } from './FlashCardAnswer';
 
+const MIN_QUESTION_POOL_SIZE = 3;
+const ANSWER_OPTION_COUNT = 4;
+
 const getRandomElement = (array) => {
   return array[Math.floor(Math.random() * array.length)]
 }
 
 const FlashCardGame = () => {
-  function nextQuestion() {
-    setCurrentCharacter(getRandomElement(allCharacters))
+
+  function resetModel() {
+    const currentCharacter = getRandomElement(allCharacters)
+    return {
+      questionPool: allCharacters,
+      correctAnswers: [],
+      incorrectAnswers: [],
+      currentCharacter,
+      answerOptions: generateAnswerOptions(currentCharacter)
+    }
+  }
+
+  function generateAnswerOptions(currentCharacter) {
+    const answerOptionIndex = Math.floor(Math.random() * ANSWER_OPTION_COUNT)
+    const answerOptionPool = allCharacters.filter(it => it.hangul !== currentCharacter.hangul);
+    const options = [];
+
+    for (let i = 0; i < ANSWER_OPTION_COUNT; i++) {
+      if (i === answerOptionIndex) {
+        options.push(currentCharacter)
+      } else {
+        const optionPoolIndex = Math.floor(Math.random() * answerOptionPool.length)
+        options.push(answerOptionPool[optionPoolIndex])
+        answerOptionPool.splice(optionPoolIndex, 1)
+      }
+    }
+
+    return options;
   }
 
   function checkAnswer(character) {
-    if (character.hangul === currentCharacter.hangul) {
-      setCorrectAnswers(correctAnswers.concat([character]))
+    if (character.hangul === model.currentCharacter.hangul) {
+      model.correctAnswers.push(model.currentCharacter)
     } else {
-      setIncorrectAnswers(incorrectAnswers.concat([character]))
+      model.incorrectAnswers.push(model.currentCharacter)
     }
+
     nextQuestion()
   }
 
+  function nextQuestion() {
+    if (model.questionPool.length < MIN_QUESTION_POOL_SIZE) {
+      model.questionPool = allCharacters
+    }
+    model.questionPool = model.questionPool.filter(it => it.hangul !== model.currentCharacter.hangul)
+    model.currentCharacter = getRandomElement(model.questionPool)
+    model.answerOptions = generateAnswerOptions(model.currentCharacter)
+    setModel({...model})
+  }
+
   const allCharacters = [...hangul.simple_consonants, ...hangul.simple_vowels]
-  const [correctAnswers, setCorrectAnswers] = useState([])
-  const [incorrectAnswers, setIncorrectAnswers] = useState([])
-  let [currentCharacter, setCurrentCharacter] = useState(getRandomElement(allCharacters))
-  const options = [currentCharacter, getRandomElement(allCharacters), getRandomElement(allCharacters), getRandomElement(allCharacters)]
+  const [model, setModel] = useState(resetModel());
 
   return (
-    <div>
+     <div>
       FlashCardGame
-      <FlashCard character={currentCharacter}></FlashCard>
+      <FlashCard character={model.currentCharacter}></FlashCard>
 
       <div>
       {
-        options.map((it, i) => <FlashCardAnswer onClick={() => checkAnswer(it)} key={i} character={it}></FlashCardAnswer>)
+        model.answerOptions.map((it, i) => <FlashCardAnswer onClick={() => checkAnswer(it)} key={i} character={it}></FlashCardAnswer>)
       }
       </div>
+
+      {!!model.currentCharacter && process.env.NODE_ENV === 'development' &&
+      (
+        <div>
+          <div>
+            Answer {model.currentCharacter.roman} {model.currentCharacter.hangul}
+          </div>
+
+          Question pool ({model.questionPool.length})
+          <div>
+            {model.questionPool.map((it, i) => <span key={i}>{it.hangul}</span>)}
+          </div>
+
+          Correct answers ({model.correctAnswers.length})
+          <div>
+            {model.correctAnswers.map((it, i) => <span key={i}>{it.hangul}</span>)}
+          </div>
+
+          Incorrect answers ({model.incorrectAnswers.length})
+          <div>
+            {model.incorrectAnswers.map((it, i) => <span key={i}>{it.hangul}</span>)}
+          </div>
+        </div>
+      
+      )}
     </div>
   );
 };
